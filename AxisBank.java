dd3685e4aed5787d5cb9a6e29df922610fed569c
BBalamanikandan <71202575+BBalamanikandan@users.noreply.github.com>
2020-09-26 14:09:13 +0530

    Week3-Day1
    Java-OOPS-Home Assignments

---
+++ AxisBank.java
@@ -0,0 +1,15 @@
+package week3.day1.homeassignments;
+
+public class AxisBank extends BankInfo {
+	public void deposit() {
+		// TODO Auto-generated method stub
+		System.out.println("Axis Bank - Deposit Account");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AxisBank ab=new AxisBank();
+		ab.deposit();
+	}
+
+}
